8fd85492e30905b9ad9a7781a962efbe39e1caa9
sergiorgscout92 <saromero058@soy.sena.edu.co>
2023-06-22 21:31:54 -0500

    Agregar materias

---
+++ Materias/pages/agregar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
+	<title>Registro de materias</title>
+</head>
+<body>
+	<div class="container">
+		<div class="col-lg-9">
+			<form action="../controladores/agregarmaterias.php">
+				<h1>Registro de materias</h1>
+				<div class="form-group">
+					<label>Nombre materia</label>
+					<input type="text" name="txtnombremate" placeholder="Ingresar nombre de la materia">
+				</div>
+				<div class="form-group">
+					<button class="btn btn-primary" type="submit" value="Registar">Registrar</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
