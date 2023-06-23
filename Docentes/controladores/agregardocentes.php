b4f117a595ece084b6427b47772906dcd6902f22
sergiorgscout92 <saromero058@soy.sena.edu.co>
2023-06-22 20:58:01 -0500

    Controlador docentes

---
+++ Docentes/controladores/agregardocentes.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../../Conexion.php');
+require_once('../modelos/docente.php');
+
+if($_POST){
+//crear objeto de la clase docente
+	$docen = new Docente();
+	$Nombredoc = $_POST['txtnombredoc'];
+	$Apellidodoc = $_POST['txtapellidodoc'];
+	$Documentodoc = $_POST['txtdocumentodoc'];
+	$Correodoc = $_POST['txtcorreodoc'];
+	$Materiadoc = $_POST['txtmateriadoc'];
+	$Usuariodoc = $_POST['txtusuariodoc'];
+	$Passworddoc = MD5($_POST['txtcontrasenadoc']);
+	$Perfildoc = $_POST['txtperfildoc'];
+	$Estadodoc = $_POST['txtestadodoc'];
+
+	$docen->agregarDoc($Nombredoc,$Apellidodoc,$Documentodoc,$Correodoc,$Materiadoc,$Usuariodoc,$Passworddoc,$Perfildoc,$Estadodoc);
+}
+?>
