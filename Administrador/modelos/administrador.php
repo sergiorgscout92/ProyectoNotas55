9769eb975e7aaed3c1a2b4e2ef13022a0f363aed
sergiorgscout92 <saromero058@soy.sena.edu.co>
2023-06-22 20:53:06 -0500

    Administrador corregido

--- Administrador/modelos/administrador.php
+++ Administrador/modelos/administrador.php
@@ -1,20 +1,20 @@
 <?php
 include_once('../../Conexion.php');
 class Administrador extends Conexion{
-	public funcion __construct(){
+	public function __construct(){
 		$this->db = parent::__construct();
 	}
 
-	public fuction agregarad($Nombread,$Apellidoad,$Usuarioad,$Passwordad,$Perfilad,$Estadoad){
+	public function agregarad($Nombread,$Apellidoad,$Usuarioad,$Passwordad,$Perfilad,$Estadoad){
 
-		$statement = $this->db->prepare("INSERT INTO usuarios(Nombreusu,Apellidousu,Usuario,Passwordusu,Perfil,Estado)values(:Nombread,:Apellidoad:;Usuarioad,:Passwordad,:'Administrador',:'Activo')");
+		$statement = $this->db->prepare("INSERT INTO usuarios(Nombreusu,Apellidousu,Usuario,Passwordusu,Perfil,Estado)values(:Nombread,:Apellidoad,:Usuarioad,:Passwordad,:Perfilad,:Estadoad)");
 
-		$statement->bindParam(":Nombread",$Nombread);
-		$statement->bindParam(":Apellidoad",$Apellidoad);
-		$statement->bindParam(":Usuarioad",$Usuarioad);
-		$statement->bindParam(":Passwordad",$Passwordad);
-		$statement->bindParam(":Perfilad",$Perfilad);
-		$statement->bindParam(":Estadoad",$Estadoad);
+		$statement->bindParam(':Nombread',$Nombread);
+		$statement->bindParam(':Apellidoad',$Apellidoad);
+		$statement->bindParam(':Usuarioad',$Usuarioad);
+		$statement->bindParam(':Passwordad',$Passwordad);
+		$statement->bindParam(':Perfilad',$Perfilad);
+		$statement->bindParam(':Estadoad',$Estadoad);
 		if($statement->execute()){
 			echo "Usuario registrado";
 			header('Location: ../pages/index.php');
@@ -72,7 +72,7 @@ class Administrador extends Conexion{
 		$statement->bindParam(':Id',$Id);
 		if ($statement->execute()) {
 			echo "Usuario eliminado";
-			header('Location: ../pages/index.php')
+			header('Location: ../pages/index.php');
 		}else{
 			echo "El usuario no se pudo eliminar";
 			header('Location: ../pages/eliminar.php');
