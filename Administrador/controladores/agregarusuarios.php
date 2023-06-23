4c62e4d87393b4496eaa0211fc919db5e49a8ddc
sergiorgscout92 <saromero058@soy.sena.edu.co>
2023-06-22 20:57:14 -0500

    Controlador administrador

---
+++ Administrador/controladores/agregarusuarios.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../../Conexion.php');
+require_once('../modelos/administrador.php');
+
+if($_POST){
+//crear objeto de la clase administrador
+	$admin = new Administrador();
+	$Nombreusu = $_POST['txtnombre'];
+	$Apellidousu = $_POST['txtapellido'];
+	$Usuariousu = $_POST['txtusuario'];
+	$Passwordusu = MD5($_POST['txtcontrasena']);
+	$Perfilusu = $_POST['txtperfil'];
+	$Estadousu = $_POST['txtestado'];
+
+	$admin->agregarad($Nombreusu,$Apellidousu,$Usuariousu,$Passwordusu,$Perfilusu,$Estadousu);
+}
+?>
