d6d05b05b6429f652f93bf37979907af3e38b95c
sergiorgscout92 <saromero058@soy.sena.edu.co>
2023-06-22 20:59:24 -0500

    index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<title>Inicio de sesión</title>
+</head>
+<body>
+	<div action="" method="POST">
+		<div class="container">
+			<div class="col-lg-9">
+				<h1>Inicio de sesión</h1>
+				<div class="form-group">
+					<label>Usuario</label>
+					<br>
+					<input type="text" name="txtUsuario" placeholder="Ingrese su usuario">
+				</div>			
+				<div class="form-group">
+					<br>
+					<label>Contraseña</label>
+					<br>
+					<input type="password" name="txtPass" placeholder="Ingrese su contraseña">
+				</div>
+				<div class="form-group">
+					<br>
+					<input class="btn btn-primary" type="submit" value="Iniciar sesión" name="iniciarSesion">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
